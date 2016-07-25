1463a9dbb82d8a6c29a8ef094796f7efa52e19ed
Roi Driscoll <pollocknoah@gmail.com>
2016-07-25 13:38:23 +0100

    updates e-mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
   # Disable request forgery protection in test environment.
   config.action_controller.allow_forgery_protection = false
   config.action_mailer.default_url_options = {host: 'example.com'}
+  Rails.application.routes.default_url_options[:host] = 'http://uoi.herokuapp.com'
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
